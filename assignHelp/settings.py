31a3a115e9c98ed7f2a1c1e23ece5d8a63e1aaaf
Manish Kunwar <ranaxmond@gmail.com>
2021-12-22 15:20:50 +0545

    update

--- assignHelp/settings.py
+++ assignHelp/settings.py
@@ -51,10 +51,10 @@ ALLOWED_HOSTS = ["*"]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
