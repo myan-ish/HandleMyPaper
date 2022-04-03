baed4b678ddf41b88ea17b0ab8e449eee49636c2
Manish Kunwar <ranaxmond@gmail.com>
2022-04-03 16:46:23 +0545

    Update settings.py

--- assignHelp/settings.py
+++ assignHelp/settings.py
@@ -52,9 +52,9 @@ ALLOWED_HOSTS = ["*"]
 SITE_ID = 1
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
